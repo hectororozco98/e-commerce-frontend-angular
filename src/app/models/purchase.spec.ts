491b5a4fb39663e568807f4c50d5e46c9699fb05
Jen Anderson <jander38@yahoo.com>
2022-07-30 20:54:44 -0400

    unfocused test and added purchase spec

---
+++ src/app/models/purchase.spec.ts
@@ -0,0 +1,15 @@
+// import { Purchase } from './purchase';
+
+// describe('Purchase', () => {
+//     it('should create a new Purchase', () => {
+//       let purchase = new Purchase(1, "order1",[],[]);
+//       expect(purchase).toBeTruthy();
+      
+//       expect(purchase.id).toEqual(1);
+//       expect(purchase.orderPlaced).toEqual("order1");
+//       expect(purchase.ownerUser).toEqual([]);
+//       expect(purchase.product).toEqual([]); 
+     
+  
+//     });
+// });
