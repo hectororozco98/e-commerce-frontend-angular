487272d998620191afad56f4862f7912ff7bb23c
Jen Anderson <jander38@yahoo.com>
2022-07-30 21:05:55 -0400

    added testing for address service

--- src/app/services/address.service.spec.ts
+++ src/app/services/address.service.spec.ts
@@ -1,16 +1,42 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { AddressService } from './address.service';
 
+
 describe('AddressService', () => {
-  let service: AddressService;
+  let service: AddressService,
+  httpMock: HttpTestingController;
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        AddressService
+      ]
+    });
     service = TestBed.inject(AddressService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  afterEach(()=>{
+    httpMock.verify();
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('get user addresses', ()=>{
+    it('should return exp')
+  })
+
+  describe('add address', ()=>{})
+
+  describe('update address', ()=>{})
+
+
+
+
 });
