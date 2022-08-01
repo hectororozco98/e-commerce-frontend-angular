ef659d82e9bca8bcd6d75ec20d38a5003d968720
Jen Anderson <jander38@yahoo.com>
2022-07-31 20:43:29 -0400

    added testing for searchbar component

--- src/app/components/searchbar/searchbar.component.spec.ts
+++ src/app/components/searchbar/searchbar.component.spec.ts
@@ -1,24 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Product } from 'src/app/models/product';
+import { of } from 'rxjs';
+import { AppComponent } from 'src/app/app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, FormBuilder, NgControl } from '@angular/forms';
+import { AppRoutingModule } from './../../app-routing.module';
+import { ProductService } from 'src/app/services/product.service';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { SearchbarComponent } from './searchbar.component';
+import { faShirt } from '@fortawesome/free-solid-svg-icons';
 
-describe('SearchbarComponent', () => {
+fdescribe('SearchbarComponent', () => {
   let component: SearchbarComponent;
   let fixture: ComponentFixture<SearchbarComponent>;
+  let service: ProductService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SearchbarComponent],
+      declarations: [SearchbarComponent, AppComponent],
+      providers: [ProductService, FormBuilder, AppComponent],
+      imports: [AppRoutingModule, FormsModule, HttpClientModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = TestBed.inject(ProductService);
   });
 
-  it('should create', () => {
+  // it('should create', () => {
 	
-    expect(component).toBeTruthy();
-  });
+  //   expect(component).toBeTruthy();
+  // });
+
+  it('subscribe test for searching a product', fakeAsync(() => {
+    let product = new Product(10, 'p1', 5, 'p1', 15, ''),
+         searchTerm = component.searchTerm,
+        spy = spyOn(service, 'getSearchProducts').and.returnValue(of([])),
+        subSpy = spyOn(service.getSearchProducts(product.toString()), 'subscribe');
+    component.ngOnInit();
+    tick(); 
+    expect(spy).toHaveBeenCalledBefore(subSpy);
+    expect(subSpy).toHaveBeenCalled();
+  }));
+
+  it('test for inside subscribe method', fakeAsync(() => {
+    let product = new Product(1, 'p1', 5, 'p1', 15, '');
+   
+    let spy = spyOn(service, "getSearchProducts").and.returnValue(of([]));
+
+    component.ngOnInit();
+    tick();
+    expect(component.searchTerm).toBeDefined();
+    expect(component.search.length).toEqual(1);
+    expect(component["appComponent"].isSearching).toEqual(true);
+  }))
+
+
+
+
+
 
 });
